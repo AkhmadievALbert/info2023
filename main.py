fe327b45c1524f5c0af73d6206631ceb03571d47
AkhmadievALbert <a.akhmadiev@tinkoff.ru>
2023-03-23 19:58:07 +0300

    Task5

--- main.py
+++ main.py
@@ -1,167 +1,106 @@
-from spider import Spider
-
-# if __name__ == '__main__':
-#     base_url = "https://www.crummy.com/software/BeautifulSoup/bs4/doc.ru/bs4ru.html"
-#     nested_link_class = "reference internal"
-#     spider = Spider(base_url, nested_link_class)
-#     spider.start_parsing()
-
-import requests
-from bs4 import BeautifulSoup
-import re
+import json
 import os
-import pymorphy2
-import nltk
-from stop_words import get_stop_words
-nltk.download('punkt')
+import tools
+from tokenizer import Tokenizer
+from spider import Spider
+from inverted_index import InvertedIndexFactory
+from boolean_search import BooleanSearch
+from tf_idf_calculator import TF_IDF_Calculator
+from vector_model_search import VectorModelSearch
 
-# Инициализируем анализатор морфологии
-morph = pymorphy2.MorphAnalyzer()
 
-# Список стоп-слов (союзов, предлогов, чисел)
-# stop_words = ['без', 'бы', 'был', 'была', 'были', 'было', 'быть', 'в', 'вам', 'вас', 'весь', 'во', 'вот', 'все', 'всего', 'всей', 'вы', 'да', 'для', 'до', 'его', 'ее', 'ей', 'ему', 'если', 'есть', 'еще', 'же', 'за', 'и', 'из', 'или', 'им', 'их', 'к', 'как', 'ко', 'когда', 'кто', 'ли', 'либо', 'между', 'меня', 'мне', 'много', 'может', 'мы', 'на', 'над', 'нам', 'нами', 'нас', 'наш', 'не', 'него', 'нее', 'ней', 'нет', 'ни', 'них', 'но', 'ну', 'о', 'об', 'однако', 'он', 'она', 'они', 'оно', 'от', 'очень', 'по', 'под', 'при', 'с', 'со', 'так', 'также', 'такой', 'те', 'тебя', 'тем', 'то', 'того', 'тоже', 'той', 'только', 'том', 'ты', 'у', 'уже', 'хотя', 'чего', 'чей', 'чем', 'что', 'чтобы', 'чье', 'чья', 'эта', 'эти', 'это', 'я']
-stop_words = get_stop_words('ru')
-urls = []
-def fill_urls():
-    for i in range(1, 11):
-        urls.append(f"https://habr.com/ru/post/{i}")
+def run_spider():
+    print("-> Starting spider...")
+    spider = Spider(tools.BASE_URL, tools.NESTED_LINK_REGEXP)
+    spider.start_parsing()
 
-def get_contents():
-    # Создаем директорию для файлов, если она не существует
-    if not os.path.exists('output'):
-        os.makedirs('output')
 
-    # Список страниц для загрузки
+def run_tokenizer():
+    print("\n-> Starting tokenizer...")
+    input_directory_path = tools.TEXT_DOCUMENTS_PATH
+    output_directory_path = tools.LEMMATIZED_TEXTS_PATH
 
-    # Создаем файл index.txt
-    with open('index.txt', 'w', encoding='utf-8') as f:
-        for i, url in enumerate(urls):
-            # Получаем содержимое страницы
-            response = requests.get(url)
-            content = response.content
+    all_files = os.listdir(input_directory_path)
+    tools.prepare_output_directory(output_directory_path)
 
-            # Парсим содержимое страницы и сохраняем его в файл
-            soup = BeautifulSoup(content, 'html.parser')
-            text = soup.get_text().lower()
-            text = re.sub(r'\s+', ' ', text)  # удаляем лишние пробелы и переносы строк
-            filename = f'output/page{i + 1}.txt'
-            with open(filename, 'w', encoding='utf-8') as page_file:
-                page_file.write(text)
+    tokenizer = Tokenizer()
 
-            # Записываем номер файла и ссылку на страницу в файл index.txt
-            f.write(f'{i + 1}. {url}\n')
+    for filename in all_files:
+        input_file_path = input_directory_path + '/' + filename
+        with open(input_file_path, 'r') as file:
+            text = file.read().replace('\n', '')
+            cleaned_text = tokenizer.clean_text(text)
+            output_file_path = output_directory_path + '/' + filename
+            tools.save_text_in_file(output_file_path, ' '.join(cleaned_text))
+            print('Done %s' % filename)
 
-# Функция для получения списка токенов из файла
-def get_tokens(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
-        text = f.read()
-        # Удаляем лишние символы
-        text = re.sub(r'\W+', ' ', text)
-        # Токенизируем текст
-        tokens = nltk.word_tokenize(text)
-        # Отфильтровываем стоп-слова и "мусор"
-        tokens = [token for token in tokens if token not in stop_words and re.match(r'^[а-яА-Я]+$', token) and not any(char.isdigit() for char in token)]
-        # Лемматизируем токены
-        # lemmas = [morph.parse(token)[0].normal_form for token in tokens]
-        # Оставляем только уникальные леммы
-        unique_lemmas = set(tokens)
-        return list(unique_lemmas)
+    print('Done')
 
-inverted_index = {}
-def get_lemmas():
-    tokens = []
-    for i, url in enumerate(urls):
-        filename = os.path.join("output", 'page{}.txt'.format(i + 1))
-        # Получаем список токенов из файла
-        file_tokens = get_tokens(filename)
-        # Добавляем токены в общий список
-        tokens += file_tokens
-        for token in file_tokens:
-            lemma = morph.parse(token)[0].normal_form
-            if lemma in inverted_index:
-                inverted_index[lemma].add(i + 1)
-            else:
-                inverted_index[lemma] = {i + 1}
 
-    tokens = set(tokens)
-    # Группируем токены по леммам
-    lemmas = {}
-    for token in tokens:
-        lemma = morph.parse(token)[0].normal_form
-        if lemma in lemmas:
-            lemmas[lemma].append(token)
-        else:
-            lemmas[lemma] = [token]
+def run_inverted_index():
+    print("\n-> Starting inverted index...")
+    factory = InvertedIndexFactory(tools.LEMMATIZED_TEXTS_PATH, tools.INVERTED_INDEX_PATH)
+    factory.make_inverted_index()
+    print('Inverted index was created!')
 
-    with open('inverted_index.txt', 'w', encoding='utf-8') as f:
-        for key in sorted(inverted_index.keys()):
-            f.write(key + ': ' + ', '.join(f'{inverted_index[key]}') + '\n')
 
-    with open('tokens.txt', 'w', encoding='utf-8') as f:
-        for token in sorted(tokens):
-            f.write(token + ' ')
+def run_boolean_search():
+    all_files_indexes = [filename[:-4] for filename in os.listdir(tools.LEMMATIZED_TEXTS_PATH)]
 
-    with open('grouped_tokens.txt', 'w', encoding='utf-8') as f:
-        for lemma in sorted(lemmas.keys()):
-            f.write(lemma + ': ' + ', '.join(lemmas[lemma]) + '\n')
+    bs = BooleanSearch(tools.INVERTED_INDEX_PATH, all_files_indexes)
 
-def create_inverted_index():
-    for i, url in enumerate(urls):
-        filename = os.path.join("output", 'page{}.txt'.format(i + 1))
-        with open(filename, 'r', encoding='utf-8') as f:
-            text = f.read()
-            tokens = re.findall(r'\b\w+\b', text.lower())
-            for token in set(tokens):
-                if token.isdigit():
-                    continue
-                if token in stop_words:
-                    continue
-                if not token.isalpha():
-                    continue
-                if any(c.isdigit() for c in token):
-                    continue
-                lemma = morph.parse(token)[0].normal_form
-                print(inverted_index)
-                if lemma in inverted_index:
-                    inverted_index[lemma].add(i + 1)
-                else:
-                    inverted_index[lemma] = {i + 1}
-    with open('inverted_index.txt', 'w', encoding='utf-8') as f:
-        for key in sorted(inverted_index.keys()):
-            f.write(key + ': ' + ', '.join(inverted_index[key]) + '\n')
+    queries = {
+        "байден & навальный | !путин",
+        "байден & навальный | путин",
+        "рецепт & вкусного & блина",
+        "война & США | Россия",
+        "политика & европа & санкции"
+    }
 
-# Реализуем булев поиск
-def boolean_search(query):
-    query = query.lower().split()
-    results = set(range(1, len(urls) + 1))
-    for term in query:
-        if term == 'and':
-            continue
-        elif term == 'or':
-            continue
-        elif term == 'not':
-            continue
-        else:
-            if term in inverted_index:
-                results &= inverted_index[term]
-            else:
-                results &= set()
-    for i, term in enumerate(query):
-        if term == 'and':
-            if query[i-1] in inverted_index and query[i+1] in inverted_index:
-                results &= inverted_index[query[i-1]] & inverted_index[query[i+1]]
-        elif term == 'or':
-            if query[i-1] in inverted_index and query[i+1] in inverted_index:
-                results |= inverted_index[query[i-1]] | inverted_index[query[i+1]]
-        elif term == 'not':
-            if query[i+1] in inverted_index:
-                results -= inverted_index[query[i+1]]
-    return sorted(results)
+    for query in queries:
+        bs.search(query)
+
+
+def run_tf_idf_calculator():
+    print("\n-> Starting TF-IDF calculator...")
+    all_filenames = os.listdir(tools.LEMMATIZED_TEXTS_PATH)
+
+    with open(tools.INVERTED_INDEX_PATH) as json_file:
+        inverted_index = json.load(json_file)
+
+    result = {}
+
+    for term in inverted_index.keys():
+        docs_with_term = inverted_index[term]
+        for doc_index in docs_with_term:
+            lem_file_path = tools.LEMMATIZED_TEXTS_PATH + "/" + doc_index + ".txt"
+
+            with open(lem_file_path, 'r') as file:
+                tokens = file.read().split(' ')
+
+            TF, IDF, TF_IDF = TF_IDF_Calculator.calculate(term,
+                                                          tokens,
+                                                          len(all_filenames),
+                                                          len(docs_with_term))
+
+            try:
+                result[term][doc_index] = {"TF": TF, "IDF": IDF, "TF-IDF": TF_IDF}
+            except KeyError:
+                result[term] = {doc_index: {"TF": TF, "IDF": IDF, "TF-IDF": TF_IDF}}
+
+    dump = json.dumps(result,
+                      sort_keys=False,
+                      indent=4,
+                      ensure_ascii=False,
+                      separators=(',', ': '))
+
+    tools.save_text_in_file(tools.TF_IDF_PATH, dump)
+
 
 if __name__ == '__main__':
-    fill_urls()
-    # get_contents()
-    get_lemmas()
-    # create_inverted_index()
-    print(inverted_index)
-    print(boolean_search("следующий and сайт"))
+    # run_spider()
+    # run_tokenizer()
+    # run_inverted_index()
+    # run_tf_idf_calculator()
+    vms = VectorModelSearch()
+    vms.search("Гамма излучение")
--- spider.py
+++ spider.py
@@ -1,9 +1,13 @@
+import json
+
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 
 import os
+import tools
 import shutil
 import urllib.request
+import re
 
 
 def tag_visible(element):
@@ -20,30 +24,33 @@ def get_html(url):
     :param url: URL сайта
     :return: HTML документ
     """
-    response = urllib.request.urlopen(url)
-    return response.read()
+    try:
+        response = urllib.request.urlopen(url)
+        return response.read()
+    except:
+        return None
 
 
 class Spider:
 
     def __init__(self,
                  base_url,
-                 nested_link_class,
+                 nested_link_regexp,
                  max_pages_count=100,
-                 min_words_count=500,
+                 min_words_count=1200,
                  output_directory="output/",
-                 output_filename="index.txt"):
+                 output_filename="index.json"):
         """
         Конструктор
         :param base_url: Базовый URL, с которого начинается работа краулера
-        :param nested_link_class: Класс, по которому краулер будет искать вложенные ссылки
+        :param nested_link_regexp: Регулярка
         :param max_pages_count: Максимальное количество обработанных страниц
         :param min_words_count: Минимальное кол-во слов на странице
         :param output_directory: Директория для сохранения документов
         :param output_filename: Имя файла, в который записывается индекс страницы и ее URL
         """
         self.__base_url = base_url
-        self.__nested_link_class = nested_link_class
+        self.__nested_link_regexp = nested_link_regexp
         self.__max_pages_count = max_pages_count
         self.__min_words_count = min_words_count
         self.__output_directory = output_directory
@@ -51,37 +58,41 @@ class Spider:
         self.__current_page_index = 0
         self.__queue = []
         self.__parsed_urls = set()
+        self.__index = {}
+        self.html_documents_path = "%s/text_documents/" % self.__output_directory
 
     def start_parsing(self):
-        """ Производит парсинг с начальный страницы """
-        self.__prepare_output_directory()
+        """ Производит парсинг с начальной страницы """
+        tools.prepare_output_directory(self.__output_directory)
+
+        os.mkdir(self.html_documents_path)
+
         self.__queue.append(self.__base_url)
 
         while self.__queue and self.__current_page_index < self.__max_pages_count:
             url = self.__queue.pop()
             html = get_html(url)
+
+            if html is None:
+                continue
+
             soup = BeautifulSoup(html, 'html.parser')
+            words_list = self.__get_visible_words_list(soup)
 
-            print('Start handling %d %s ...' % (self.__current_page_index, url))
+            self.__parsed_urls.add(url)
+            print('Processing %s ...' % url)
 
-            if self.__check_text_size(soup):
-                self.__save_html(self.__current_page_index, url, html)
-                self.__parsed_urls.add(url)
+            if len(words_list) >= self.__min_words_count:
+                text = u" ".join(t.strip() for t in words_list)
+                self.__save_text(self.__current_page_index, url, text)
                 self.__current_page_index += 1
-                nested_links = list(filter(self.__is_handled, self.__get_nested_links(soup)))
-                self.__queue.extend(nested_links)
+                print('Saved {} - {}'.format(self.__current_page_index, url))
 
+            nested_links = list(filter(self.__is_handled, self.__get_nested_links(soup)))
+            self.__queue.extend(nested_links)
+
+        self.__save_index()
         print("Done!")
-
-    def __prepare_output_directory(self):
-        """ Очищает папку output от файлов предыдущего запуска """
-        try:
-            shutil.rmtree(self.__output_directory)
-            os.mkdir(self.__output_directory)
-        except OSError:
-            print("Creation of the directory %s failed" % self.__output_directory)
-        else:
-            print("Successfully created the directory '%s' " % self.__output_directory)
 
     def __is_handled(self, url):
         return not (url in self.__parsed_urls)
@@ -90,34 +101,38 @@ class Spider:
         """
         Возвращает массив вложенных ссылок
         """
-        internal_references = soup.find_all("a", class_=self.__nested_link_class)
+        internal_references = soup.findAll('a', attrs={'href': re.compile(self.__nested_link_regexp)})
         links = list(set([self.__base_url + item['href'] for item in internal_references]))
         return links
 
-    def __check_text_size(self, soup):
+    @staticmethod
+    def __get_visible_words_list(soup):
         """
         Проверяет количество слов на странице
         :return: True, если слов не меньше self.__min_words_count
         """
-        size = len(soup.text.split())
-        return size >= self.__min_words_count
+        texts = soup.findAll(text=True)
+        visible_texts = list(filter(tag_visible, texts))
+        return visible_texts
 
-    def __save_html(self, index, url, html):
+    def __save_text(self, index, url, text):
         """
-        Созхраняет переданный HTML документ и
+        Сохраняет переданный текстовый документ и
         добавляет в таблицу URL с индексом
         :param index: Индекс страницы
         :param index: URL страницы
-        :param html: HTML документ
+        :param text: HTML документ
         """
 
-        html_filename_path = self.__output_directory + str(index) + ".txt"
-        html_file = open(html_filename_path, "wb")
-        html_file.write(html)
-        html_file.close()
+        html_filename_path = "%s/%d.txt" % (self.html_documents_path, index)
+        tools.save_text_in_file(html_filename_path, text)
+        self.__index[index] = url
 
-        output_filename_path = self.__output_directory + self.__output_filename
+    def __save_index(self):
+        dump = json.dumps(self.__index,
+                          sort_keys=False,
+                          indent=4,
+                          ensure_ascii=False,
+                          separators=(',', ': '))
 
-        with open(output_filename_path, 'a') as file:
-            line = str(index) + " – " + url + "\n"
-            file.write(line)
+        tools.save_text_in_file(self.__output_directory + self.__output_filename, dump)
